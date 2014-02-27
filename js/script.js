ddec7f6e5ad45a9a3d0a7b904f3631a5f816aae0
liam m mahady arch student <lmm28@afsconnect1.njit.edu>
2014-02-26 21:30:37 -0500

    Added field for operator and the ability to swipe digits and operators into their fields

--- js/script.js
+++ js/script.js
@@ -4,8 +4,43 @@ $(function()
   var zoom3=new ZoomView('#zoom3','#zoom3 :first');
   var carousel1=new swipable('#carousel1');
   var myCarousel=new swipable('#myCarousel');
+  var digit1=new swipeDigit('#digit1', '1');
+  var digit2=new swipeDigit('#digit2','2');
+  var digit3=new swipeDigit('#digit3','3');
+  var digit4=new swipeDigit('#digit4','4');
+  var digit5=new swipeDigit('#digit5', '5');
+  var digit6=new swipeDigit('#digit6','6');
+  var digit7=new swipeDigit('#digit7','7');
+  var digit8=new swipeDigit('#digit8','8');
+  var digit9=new swipeDigit('#digit9','9');
+  var digit0=new swipeDigit('#digit0','0');
+  var opPlus=new swipeOp('#opPlus','+');
+  var opMinus=new swipeOp('#opMinus','-');
+  var opMult=new swipeOp('#opMult','x');
+  var opDiv=new swipeOp('#opDiv','/');
   }
  );
+function swipeDigit(digit, value)
+  {digit=$(digit).hammer({prevent_default:true});
+  digit.bind
+    ('swipeleft', function(event)
+      {$('#leftOp').append(value);
+      }
+    );
+  digit.bind
+    ('swiperight', function(event)
+      {$('#rightOp').append(value);
+      }
+    );
+  }
+function swipeOp(op, value)
+  {op=$(op).hammer({prevent_default:true});
+  op.bind
+    ('swipedown', function(event)
+      {$('#opDisplay').text(value);
+      }
+    );
+  }
 function swipable(carousel)
   {carousel=$(carousel).hammer({prevent_default:true});
   carousel.bind
